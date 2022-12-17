fa628c39997cffc1e0f58918ae9c4fa87988d843
Lukas Schauhuber <35468672+luggaesch@users.noreply.github.com>
2022-12-17 13:08:46 +0100

    added joker

--- types/joker.ts
+++ types/joker.ts
@@ -1,12 +1,11 @@
-import Match from "./match";
-
 export enum Jokers {
     "Wikipedia"= "Wikipedia",
     "DoubleDown" = "Doppelt oder Nichts",
     "Teamwork" = '"Teamwork"',
     "VierGewinnt" = "Vier Gewinnt",
     "Ueberfall" = "Hände Hoch",
-    "Telefon" = "Call me maybe"
+    "Telefon" = "Call me maybe",
+    "Lehrerliebling" = "Lehrerliebling"
 }
 
 type Joker = {
